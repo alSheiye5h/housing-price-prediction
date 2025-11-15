07d77f9102018ebbbc735742b0d2e821b0c40080
alSheiye5h <anasbelkhadir13@gmail.com>
2025-11-15 20:54:59 +0100

    _

--- main.py
+++ main.py
@@ -66,6 +66,93 @@ cost = compute_cost(X_train, y_train, test_w, test_b)
 
 print('Cost at test w and b (non-zeros): {:.3f}'.format(cost))
 
+def compute_gradient(X, y, w, b, *argv): 
+    """
+    Computes the gradient for logistic regression 
+ 
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
+    """
+    m, n = X.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
 
-# UNIT TESTS
-compute_cost_test(compute_cost)
+    for i in range(m):
+        # Compute z = w*x + b
+        z_wb = np.dot(X[i], w) + b
+
+        # Apply sigmoid
+        f_wb = sigmoid(-z_wb)
+
+        # Compute partial derivatives for this example
+        err = f_wb - y[i]
+        dj_db += err
+        dj_dw += err * X[i]
+    
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_db, dj_dw
+
+initial_w = np.zeros(n)
+initial_b = 0.
+
+dj_db, dj_dw = compute_gradient(X_train, y_train, initial_w, initial_b)
+print(f'dj_db at initial w and b (zeros):{dj_db}' )
+print(f'dj_dw at initial w and b (zeros):{dj_dw.tolist()}' )
+
+test_w = np.array([ 0.2, -0.5])
+test_b = -24
+dj_db, dj_dw  = compute_gradient(X_train, y_train, test_w, test_b)
+
+print('dj_db at test w and b:', dj_db)
+print('dj_dw at test w and b:', dj_dw.tolist())
+
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters, lambda_): 
+    
+    # number of training examples
+    m = len(X)
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w_history = []
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = gradient_function(X, y, w_in, b_in, lambda_)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w_in = w_in - alpha * dj_dw               
+        b_in = b_in - alpha * dj_db              
+       
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(X, y, w_in, b_in, lambda_)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0 or i == (num_iters-1):
+            w_history.append(w_in)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w_in, b_in, J_history, w_history 
+
+np.random.seed(1)
+initial_w = 0.01 * (np.random.rand(2) - 0.5)
+initial_b = -8
+
+# Some gradient descent settings
+iterations = 10000
+alpha = 0.001
+
+w,b, J_history,_ = gradient_descent(X_train ,y_train, initial_w, initial_b, 
+                                   compute_cost, compute_gradient, alpha, iterations, 0)
