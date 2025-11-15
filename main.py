9a3b33f67eaa852840e6ebd7d8641e4510243f32
alSheiye5h <anasbelkhadir13@gmail.com>
2025-11-15 20:44:34 +0100

    _

--- main.py
+++ main.py
@@ -33,14 +33,6 @@ plt.legend(loc="upper right")
 
 # the sigmoid function : 
 def sigmoid(z):
-    """
-    Compute the sigmoid of z
-    Args:
-        z (ndarray): A scalar, numpy array of any size.
-
-    Returns:
-        g (ndarray): sigmoid(z), with the same shape as z
-    """
     g = 1 / (1 + np.exp(-z))
     
     return g
@@ -49,4 +41,31 @@ value = 0
 
 print (f"sigmoid({value}) = {sigmoid(value)}")
 
+def compute_cost(X, y, w, b, *argv):
 
+    m, n = X.shape
+    z = np.dot(X, w) + b
+    f_wb = sigmoid(z)
+    
+    cost = (-1 / m) * np.sum(y * np.log(f_wb) + (1 - y) * np.log(1 - f_wb))
+    
+    return cost
+
+m, n = X_train.shape
+
+# Compute and display cost with w and b initialized to zeros
+initial_w = np.zeros(n)
+initial_b = 0.
+cost = compute_cost(X_train, y_train, initial_w, initial_b)
+print('Cost at initial w and b (zeros): {:.3f}'.format(cost))
+
+# Compute and display cost with non-zero w and b
+test_w = np.array([0.2, 0.2])
+test_b = -24.
+cost = compute_cost(X_train, y_train, test_w, test_b)
+
+print('Cost at test w and b (non-zeros): {:.3f}'.format(cost))
+
+
+# UNIT TESTS
+compute_cost_test(compute_cost)
