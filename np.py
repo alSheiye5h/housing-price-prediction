d8e0b3b1d6763808e1d7bcb05f0b02ff059106e5
alSheiye5h <anasbelkhadir13@gmail.com>
2025-11-17 13:06:00 +0100

    _

--- np.py
+++ np.py
@@ -2,27 +2,27 @@ import numpy as np
 
 
 
-a = np.array([[1, 2, 3], [5, 6, 4], [1, 2, 5]])
-print(a.dtype)
+# a = np.array([[1, 2, 3], [5, 6, 4], [1, 2, 5]])
+# print(a.dtype)
 
-b = np.zeros(5)
-print(b)
+# b = np.zeros(5)
+# print(b)
 
-c = np.ones(6)
-print(c)
+# c = np.ones(6)
+# print(c)
 
-d = np.empty(5)
-print(d)
+# d = np.empty(5)
+# print(d)
 
-e = np.arange(6)
-print(e)
+# e = np.arange(6)
+# print(e)
 
-f = np.linspace(0, 10, num=4)
-print(f)
+# f = np.linspace(0, 10, num=4)
+# print(f)
 
-g = np.ones(2, dtype=np.int64)
-print(g)
+# g = np.ones(2, dtype=np.int64)
+# print(g)
 
 a = np.arange(6)
-b = np.reshape(3, 2)
+b = a.reshape(3, 2)
 print(b) 
